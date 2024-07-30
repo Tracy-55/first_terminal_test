d3cb6b353e5cdae3a8292cc3d483b12335677f1f
Tracy-55 <kellytracey0911@gmail.com>
2024-07-30 12:55:26 +0200

    initial commit

---
+++ test/greet.test.js
@@ -0,0 +1,15 @@
+
+import { expect } from 'chai';
+import greet from '../greet.js';
+
+describe('The greet function', () => {
+    it('should greet Andrew correctly', () => {
+        const message = greet('Andrew');
+        expect(message).to.equal('Hello, Andrew!');
+    });
+
+    it('should greet Karen correctly', () => {
+        const message = greet('Karen');
+        expect(message).to.equal('Hello, Karen!');
+    });
+});
